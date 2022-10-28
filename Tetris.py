ee331aad41e4bb4bff99064043b3a8787db786f2
aminedachir <dachiramine110>
2022-10-28 20:20:26 +0200

    write in the window

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
 window = Tk()
+window.geometry('500x500+500+100')
+window.title("Hello my friend")
+paragraphe = Label(window, text="Hello, user")
+paragraphe.grid(column=0,row=0)
 
-window.mainloop
+window.mainloop()
