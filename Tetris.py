3ec961579c5171471f0846bd15a7636a3c624ff4
aminedachir <dachiramine110>
2022-10-28 20:44:53 +0200

    Tetris game

--- Tetris.py
+++ Tetris.py
@@ -1,11 +1,6 @@
 from tkinter import *
 
 window = Tk()
-window.geometry('500x500+500+100')
-window.title("Hello my friend")
-paragraphe = Label(window, text="Hello, user",font=('Arial bold', 50))
-paragraphe.grid(column=0,row=0)
-btn = Button(window, text="Click me", bg="black", fg="white")
-btn.grid(column=0,row=5)
+window.title("Tetris game")
 
 window.mainloop()
