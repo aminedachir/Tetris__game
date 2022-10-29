bb50a020b05e0d5c820f09cbb8a867d5cfc0d73f
aminedachir <dachiramine110>
2022-10-29 13:03:12 +0200

    add combobox

--- Test.py
+++ Test.py
@@ -1,13 +1,19 @@
 from tkinter import *
+from tkinter.ttk import *
 
 window = Tk()
 window.geometry("500x500")
 window.minsize(200,100)
 window.maxsize(600,600)
 
-txt = Entry(window,width=20)
+combo = Combobox(window)
+combo['values'] = (1,2,3,4,5,'text')
+combo.current(1)
+combo.place(x=50,y=100)
+
+txt = Entry(window,width=20,state="disable")
 txt.place(x=10,y=10)
-txt.focus()
+#txt.focus()
 def click():
     res = "Welcome " + txt.get()
     txt1 = Label(window,text=res)
@@ -15,5 +21,7 @@ def click():
 
 btn = Button(window,text='submit',command=click)
 btn.place(x=40,y=40)
+
+
 
 window.mainloop()
